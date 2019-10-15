cc99e973756f698e7049293378f805524566d3f4
Alicia <wfei@iu.edu>
2019-10-15 18:01:46 -0400

    ADD API FILES

--- fire/public/api/Fighter/post.php
+++ fire/public/api/Fighter/post.php
@@ -1,7 +1,7 @@
 <?php
-
-// Step 0: Validation
 use Ramsey\Uuid\Uuid;
+
+// Step 0: Validate the incoming data
 $guid = Uuid::uuid4()->toString(); // i.e. 25769c6c-d34d-4bfe-ba98-e0ee856f3e7a
 
 // Step 1: Get a datase connection from our help class
@@ -9,10 +9,8 @@ $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
 $stmt = $db->prepare(
-  'INSERT INTO Fighter
-    (MemberId, FirstName, LastName, DateOfBirth, Gender, Email, Street, City,
-    State, ZipCode, StartDate, Position, RadioNum, StationNum)
-  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+  'INSERT INTO Patient (patientGuid, firstName, lastName, dob, sexAtBirth)
+  VALUES (?, ?, ?, ?, ?)'
 );
 
 $stmt->execute([
@@ -21,12 +19,8 @@ $stmt->execute([
   $_POST['lastName'],
   $_POST['dob'],
   $_POST['sexAtBirth']
-  $_POST['firstName'],
-  $_POST['lastName'],
-  $_POST['dob'],
-  $_POST['sexAtBirth']
 ]);
 
 // Step 4: Output
 header('HTTP/1.1 303 See Other');
-header('Location: ../records/?guid='.$guid);
+header('Location: ../records/?guid=' . $guid);
