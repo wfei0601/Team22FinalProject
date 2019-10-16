8b7a96369e944020f26b86ca9c135127f19b74e5
myerset37 <myerset77@gmail.com>
2019-10-16 17:37:14 -0400

    still trying to pull dummy data

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -5,7 +5,7 @@ var fighterRecordApp = new Vue({
   },
   methods: {
     fetchFighters() {
-      fetch('.../app/data/dummy_data_fighters.json')
+      fetch('data/dummy_data_fighters.json')
       .then(response => response.json())
       .then(json => { fighterRecordApp.fighters = json })
     }
