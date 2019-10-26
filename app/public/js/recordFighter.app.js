68a86b2075fc4753fec346a3882f63b4a141efe2
qiwyang <qiwyang@iu.edu>
2019-10-26 02:58:33 -0400

    Fix header and data validation

--- app/public/js/recordFighter.app.js
+++ app/public/js/recordFighter.app.js
@@ -5,6 +5,41 @@ var fighterRecordApp = new Vue({
   },
   methods: {
     handleSubmit(event) {
+      // this.recordFighter = {
+      //   FirstName: '',
+      //   LastName: '',
+      //   Gender: '',
+      //   DateOfBirth: '',
+      //   Street: '',
+      //   City: '',
+      //   State: '',
+      //   ZipCode: '',
+      //   WorkNum: '',
+      //   MobileNum: '',
+      //   StationNum: '',
+      //   RadioNum: '',
+      //   Email: ''
+      // }
+
+      if(this.recordFighter.FirstName == ''
+       || this.recordFighter.LastName == ''
+       ||this.recordFighter.Gender == ''
+       ||this.recordFighter.DateOfBirth == ''
+       ||this.recordFighter.Street == ''
+       ||this.recordFighter.City == ''
+       ||this.recordFighter.ZipCode == ''
+       ||this.recordFighter.WorkNum == ''
+       ||this.recordFighter.MobileNum == ''
+       ||this.recordFighter.StationNum == ''
+       ||this.recordFighter.RadioNum == ''
+       ||this.recordFighter.Email == ''
+       )
+       {
+        document.getElementById('error_show').style.display = "block";
+
+         return
+
+       }
 // console.log('Foo');
       fetch('api/Fighter/post.php', {
         method: 'POST',
